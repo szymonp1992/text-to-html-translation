053a1926c2a45aa57abb4b67cf00edd195cb0a28
Szymon Podstufka <74513612+szymonp1992@users.noreply.github.com>
2022-04-28 07:14:52 +0200

    Removed bug with doubling H2 paragraph in UL

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const input = document.querySelector('.input');
-const output = document.querySelector('.output');
+const input = document.querySelector(".input");
+const output = document.querySelector(".output");
 
 function addHTML(string) {
   let afterH2 = false;
-  let formatted = '';
+  let formatted = "";
   string.forEach((el) => {
-    if (!el.toLowerCase().includes('klíčové vlastnosti') && !afterH2) {
-      if (!el.toLowerCase().includes('klíčové funkce') && !afterH2) {
-        if (el === '') {
-          formatted += '';
+    if (!el.toLowerCase().includes("klíčové vlastnosti") && !afterH2) {
+      if (!el.toLowerCase().includes("klíčové funkce") && !afterH2) {
+        if (el === "") {
+          formatted += "";
         } else if (el === " ") {
           formatted += "";
         } else {
@@ -17,28 +17,30 @@ function addHTML(string) {
       }
     }
 
-    if (el.toLowerCase().indexOf('klíčové vlastnosti') !== -1 && !afterH2) {
+    if (el.toLowerCase().indexOf("klíčové vlastnosti") !== -1 && !afterH2) {
       formatted += `<h2><span style="font-size: 18px;"><strong>${el}</strong></span></h2><ul>`;
       afterH2 = true;
-    } else if (el.toLowerCase().indexOf('klíčové funkce') !== -1 && !afterH2) {
+      return;
+    } else if (el.toLowerCase().indexOf("klíčové funkce") !== -1 && !afterH2) {
       formatted += `<h2><span style="font-size: 18px;"><strong>${el}</strong></span></h2><ul>`;
       afterH2 = true;
+      return;
     }
-    if (el === '' && afterH2) {
-      formatted += '';
+    if (el === "" && afterH2) {
+      formatted += "";
     } else if (el && afterH2) {
       formatted += `<li>${el}</li>`;
     }
   });
-  if (formatted.indexOf('</li>') !== -1) {
-    formatted += '</ul>';
+  if (formatted.indexOf("</li>") !== -1) {
+    formatted += "</ul>";
   }
   return formatted;
 }
 
-input.addEventListener('input', (e) => {
+input.addEventListener("input", (e) => {
   const str = e.target.value;
-  const stringArray = str.split('\n');
+  const stringArray = str.split("\n");
   const pasteReadyHTML = addHTML(stringArray);
   output.value = pasteReadyHTML;
 });
